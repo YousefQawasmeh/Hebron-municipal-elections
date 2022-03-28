6ac8d50c8e0d9ac18bfce6265130d137b2a77fc3
Yousef Qawasmeh <151037@ppu.edu.ps>
2022-03-28 21:52:05 +0300

    Change dropdown menu to radio buttons

--- client/src/components/Reports/styles.js
+++ client/src/components/Reports/styles.js
@@ -1,18 +1,8 @@
 import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
-  // container: {
-  //     maxWidth: "800px",
-  //     margin: "0 auto",
-  //     direction: "rtl",
-  //     boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-  //     minHeight: "100vh",
-  // },
-  //   "th, td": {
-  //     padding: "5px 15px",
-  //   },
   inputField: {
-    marginLeft: "32px",
+    width: "100%",
     fontFamily: "IRANSans",
   },
 
@@ -21,15 +11,12 @@ export default makeStyles(() => ({
     borderCollapse: "collapse",
     width: "100%",
     minWidth: "500px",
-    // width: "90vw",
-
     " & td, th": {
       border: "1px solid #dddddd",
       textAlign: "right",
       padding: "3px",
       whiteSpace: "nowrap",
     },
-
     "& tr:nth-child(even)": {
       backgroundColor: "#dddddd",
     },
